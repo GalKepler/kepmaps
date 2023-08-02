7536a8f6f81a5b66b51d8071fa2aa8ef48124cba
GalKepler <galkepler@gmail.com>
2023-08-02 16:13:35 +0300

    WIP fetching files from OSF

--- kepmaps/datasets/annotation.py
+++ kepmaps/datasets/annotation.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from typing import Union
+from osfclient.models import File
+from bids.layout.layout import parse_file_entities
 from kepmaps.datasets.dataset import Dataset
 
 
@@ -34,7 +38,61 @@ class Annotation(Dataset):
         list
             List of available spaces for a given annotation.
         """
-        return list(self.tree.get(self.SUBFOLDER_NAME).get(annotation).keys())
+        destination = self.tree.get(self.SUBFOLDER_NAME).get(annotation)
+        return [
+            key
+            for key in destination.keys()
+            if not isinstance(destination.get(key), File)
+        ]
+
+    def fetch(
+        self,
+        annotation: str,
+        entities: dict = None,
+        destination: Union[str, Path] = None,
+    ) -> dict:
+        """
+        Fetch annotation from OSF project.
+
+        Parameters
+        ----------
+        annotation : str
+            Annotation to fetch.
+        entities : dict, optional
+            Entities to fetch, by default None
+        destination : Union[str,Path], optional
+            Destination folder, by default None
+
+        Returns
+        -------
+        dict
+            Dictionary of the fetched annotation.
+        """
+        spaces = entities.get("space")
+        if spaces is None:
+            spaces = self.query_available_spaces(annotation)
+        else:
+            if not isinstance(spaces, list):
+                spaces = [spaces]
+
+        annotation_data = self.tree.get(self.SUBFOLDER_NAME).get(annotation)
+        result = {}
+        for space in spaces:
+            space_result = annotation_data.get(space)
+            if "den" in entities:
+                space_result = space_result.get(entities.get("den"))
+            for key, file in space_result.items():
+                file_entities = parse_file_entities(key)
+                # check if entities match, if not, don't add to result
+                for entity, entity_value in entities.items():
+                    if entity in file_entities:
+                        if entity_value != file_entities.get(entity):
+                            space_result.pop(key)
+                            break
+                
+            result[space] = space_result
+            
+        return result
 
     @property
     def available_annotations(self) -> list:
--- kepmaps/datasets/utils.py
+++ kepmaps/datasets/utils.py
@@ -1,13 +1,16 @@
 import json
 from pathlib import Path
+from typing import Union
 
 # set up path to "data/kepmaps.json"
-KEPMAPS_CONFIG = Path(__file__).parents[2] / "data" / "kepmaps.json"
+KEPMAPS_CONFIG = Path(__file__).parents[2] / "data"
 
 
-def _get_project(project: str = None):
+def _get_project(
+    project: str = None, config_file: Path = KEPMAPS_CONFIG / "kepmaps.json"
+):
     """
-    Returns the OSF project ID. By default, it returns KepMap's.
+    Return the OSF project ID. By default, it returns KepMap's.
 
     Parameters
     ----------
@@ -15,7 +18,29 @@ def _get_project(project: str = None):
         OSF project ID, by default None
     """
     if project is None:
-        with open(KEPMAPS_CONFIG, "r") as f:
+        with open(config_file, "r", encoding="utf-8") as f:
             project = json.load(f)
             project = project.get("osf").get("project")
     return project
+
+
+def _get_path_patterns(
+    bids_config: Union[str, Path] = KEPMAPS_CONFIG / "bids.json"
+) -> list:
+    """
+    Get path patterns from a json file of BIDS configurations.
+
+    Parameters
+    ----------
+    bids_config : Union[str,Path]
+        Path to json file of BIDS configurations.
+
+    Returns
+    -------
+    list
+        List of path patterns.
+    """
+    with open(bids_config, "r", encoding="utf-8") as f:
+        bids_config = json.load(f)
+        path_patterns = bids_config.get("path_patterns")
+    return path_patterns
